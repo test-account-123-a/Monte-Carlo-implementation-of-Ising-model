21c9810c2355d419e8be173862e905033aab2cb7
0vev0 <43699772+0vev0@users.noreply.github.com>
2020-04-10 00:45:37 -0700

    Create Ising_instruction.py
    balabala

---
+++ Ising_instruction.py
@@ -0,0 +1,33 @@
+# for the functions below, we need to write arguments, return variables and a brief description for each of them.
+
+def initialize_lattice():
+''' 
+The initialization of lattice should be the first step of simulation. In this function, 
+we give the lattice a random initial state by built-in function: np.random.random(), the
+
+'''
+    return
+
+
+def spin_flip():
+'''
+Flip a spin, namely change the state of one lattice point from +1 to -1 or from -1 to +1, 
+once a time to generate a new state of lattice.
+'''
+    return
+
+
+def Monte_Carlo():
+'''
+If the old state of lattice and a new state of lattice are given, use Metropolis Algorithm to determine whether
+we should accept it or not.
+'''
+    return
+
+
+def Ising_simulation():
+'''
+
+'''
+    return
+
